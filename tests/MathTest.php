e73213ae6f473745c5420285a5b221dcc0b68686
Miles Blackwood <milesblackwoodmusic@gmail.com>
2016-09-27 16:29:09 -0400

    Undefined method for earlier versions of PHP w PHPUnit.

--- tests/MathTest.php
+++ tests/MathTest.php
@@ -8,9 +8,4 @@ class MathTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals(v\quot(-5.9, 3), -1.0);
 		$this->assertEquals(v\quot(10, -3), -3);
   }
-
-  public function testQuotErr() {
-	  $this->setExpectedException('PHPUnit_Framework_Exception');
-		$this->expectException(v\quot(15, 0));
-  }
 }
